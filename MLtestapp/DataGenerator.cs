92b12905580bf7c15a3c9bb9380f83ab2e9629c4
Luke Williams <luke.williams@enable.com>
2019-06-17 10:12:54 +0100

    Initial commit

---
+++ MLtestapp/DataGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MLtestapp
+{
+    public class Model
+    {
+        public int Id;
+        public double GeneOneScore;
+        public double GeneTwoScore;
+    }
+
+    public class FeatureModel
+    {
+        public float GeneOneScore;
+        public float GeneTwoScore;
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<FeatureModel> Project(this IEnumerable<Model> model)
+        {
+            return model.Select(o => new FeatureModel { GeneOneScore = (float)o.GeneOneScore, GeneTwoScore = (float)o.GeneTwoScore });
+        }
+    }
+
+    class DataGenerator
+    {
+        public readonly int _totalSamples = 200;
+
+        public readonly double _scoreOneMin = 0.8;
+
+        public readonly double _scoreTwoMin = 0.1;
+
+        public readonly double _scoreOneVariance = 0.05;
+        public readonly double _scoreTwoVariance = 0.4;
+
+        private readonly Random p1;
+        private readonly Random p2;
+
+        public DataGenerator()
+        {
+            p1 = new Random(980665);
+            p2 = new Random(314159);
+        }
+
+        public List<Model> Generate()
+        {
+            var result = new List<Model>();
+
+            result.Add(new Model
+            {
+                Id = -1,
+                GeneOneScore = 0.6,
+                GeneTwoScore = 0.2
+            });
+
+            for (int i = 0; i < _totalSamples; i++)
+            {
+                var sample = new Model
+                {
+                    Id = i,
+                    GeneOneScore = GenerateGeneScore(p1, _scoreOneMin, _scoreOneVariance),
+                    GeneTwoScore = GenerateGeneScore(p2, _scoreTwoMin, _scoreTwoVariance),
+                };
+
+                result.Add(sample);
+            }
+
+            return result;
+        }
+
+        public double GenerateGeneScore(Random generator, double min, double range)
+        {
+            return min + range * ((double)generator.Next(1000) / 1000.0);
+        }
+    }
+
+    public class ResultDisplay
+    {
+        public float GeneOneScore;
+        public float GeneTwoScore;
+        public float[] NormalisedFeatures { get; set; }
+        public bool PredictedLabel { get; set; }
+        public float Score { get; set; }
+    }
+}
---
+++ MLtestapp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML;
+
+namespace MLtestapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new DataGenerator();
+
+            var rawData = generator.Generate();
+
+            RunRandomisedPca(rawData);
+
+            Console.ReadLine();
+        }
+
+        static void RunRandomisedPca(List<Model> data)
+        {
+            MLContext ct = new MLContext();
+            var dataProjection = data.Project().Take(50);
+
+            IDataView trainingData = ct.Data.LoadFromEnumerable<FeatureModel>(dataProjection);
+
+            var rpcaProjection = ct.Transforms.Concatenate("Features", "GeneOneScore", "GeneTwoScore")
+                  .Append(ct.Transforms.NormalizeMeanVariance("NormalisedFeatures", "Features"))
+                .Append(ct.AnomalyDetection.Trainers.RandomizedPca(featureColumnName: "NormalisedFeatures", rank: 2));
+
+            var fitOfTrainingData = rpcaProjection.Fit(trainingData);
+
+            var transformOfTrainingData = fitOfTrainingData.Transform(trainingData);
+
+            // gene one is constrained to be within .8 and .9, and gene two between .1 and .5
+            var anomaly = new List<FeatureModel> { new FeatureModel { GeneOneScore = (float)100000, GeneTwoScore = (float)25000 } };
+
+            var iDataViewOfAnomaly = ct.Data.LoadFromEnumerable<FeatureModel>(anomaly);
+            var transformOfAnomaly = fitOfTrainingData.Transform(iDataViewOfAnomaly);
+
+            var trainingDataResults = ct.Data.CreateEnumerable<ResultDisplay>(transformOfTrainingData, reuseRowObject: false).ToList();
+            var anomalousDataResult = ct.Data.CreateEnumerable<ResultDisplay>(transformOfAnomaly, reuseRowObject: false).ToList();
+
+            Console.WriteLine("Results from transforming first 20 training data: Predicted, score, PCA co-ordinates");
+            foreach (var r in trainingDataResults.Take(20))
+            {
+                Console.WriteLine(r.PredictedLabel + ", " + r.Score + ", " + r.NormalisedFeatures[0] + ", " + r.NormalisedFeatures[1]);
+            }
+
+            // Claims this has a similar score to the other data, despite completely different co-ordinates.
+            Console.WriteLine("Results from transforming the \"anomaly\": Predicted, score, PCA co-ordinates");
+            foreach (var r in anomalousDataResult)
+            {
+                Console.WriteLine(r.PredictedLabel + ", " + r.Score + ", " + r.NormalisedFeatures[0] + ", " + r.NormalisedFeatures[1]);
+            }
+        }
+    }
+}
